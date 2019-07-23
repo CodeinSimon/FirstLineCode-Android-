3c3a7426420cb5932e4cd079e075342db8c155d9
Simon <xrnie@outlook.com>
2019-07-23 23:09:41 +0800

    This time i learned about how to store and restore data in Android. There are some differences between textbook and current situation. That is due to the version difference, DataSupport class has been substituted by LitePal and LitePalSupport class.

---
+++ DatabaseTest/app/src/main/java/com/example/databasetest/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.databasetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    private MyDatabaseHelper databaseHelper;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        databaseHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                databaseHelper.getWritableDatabase();
+            }
+        });
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase database = databaseHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                //first data
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                database.insert("Book", null, values);
+
+                //the second data
+                values.clear();
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                database.insert("Book", null, values);
+            }
+        });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase database = databaseHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 10.99);
+                database.update("Book", values, "name=?", new String[] {"The Da Vinci Code"});
+            }
+        });
+
+
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase database = databaseHelper.getWritableDatabase();
+                database.delete("Book", "pages > ?", new String[] {"500"});
+            }
+        });
+
+        Button queryData = (Button) findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase database = databaseHelper.getWritableDatabase();
+                Cursor cursor = database.query("Book", null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        // 遍历 Cursor对象，取出数据并打印
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d(TAG, "name is: " + name);
+                        Log.d(TAG, "author is: " + author);
+                        Log.d(TAG, "pages is: " + pages);
+                        Log.d(TAG, "price is: " + price);
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
+            }
+        });
+    }
+}
