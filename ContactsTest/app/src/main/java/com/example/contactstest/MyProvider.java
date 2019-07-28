1675a0cabee288525a068dd67f16d70ea5f2e37f
Simon <xrnie@outlook.com>
2019-07-28 23:33:21 +0800

    Changed DatabaseTest to learn ContentProvider

---
+++ ContactsTest/app/src/main/java/com/example/contactstest/MyProvider.java
@@ -0,0 +1,37 @@
+package com.example.contactstest;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+public class ContentProvider extends android.content.ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+}
